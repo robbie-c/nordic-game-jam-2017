4614907af043ad3ffcc5c86f7780597a5ab1cd11
Oleg Lenive <olenive@gmail.com>
2017-04-21 20:35:36 +0200

    We decided to make a game about fish instead of a game about spaceships  - Fix bug in PlayerMovement that causes the z-axis of the object to be set to zero

--- Assets/Scripts/Ships/PlayerMovement.cs
+++ Assets/Scripts/Ships/PlayerMovement.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 // Keeps track of player velocity, position and state
 public class PlayerMovement : MonoBehaviour {
 
+	// Controls
+	public string keyAccelerate = "w";
+	public string keyTurnLeft = "a";
+	public string keyTurnRight = "d";
+
+	// Player position etc
 	public Transform playerLocation;
-	public Vector2 velocity;
-	public Vector2 position;
+	public Vector3 velocity;
+	Quaternion direction;
+
+	// Player stats
+	public float acceleration = 0.001f;
+	public float turningSpeed = 10f;
+	public float dragCoefficient = 0.1f;
+	public float minimumSpeed = 0.01f;
+
+	// Player states
+	bool wantsToTurnLeft;
+	bool wantsToTurnRight;
+	bool wantsToAccelerate;
 
 	// Use this for initialization
 	void Start () {
@@ -16,21 +33,50 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		ResetControlStates();
+		if (Input.GetKey(keyTurnLeft)) wantsToTurnLeft = true;
+		if (Input.GetKey(keyTurnRight)) {
+			wantsToTurnRight = true;
+		}
+		if (Input.GetKey(keyAccelerate)) wantsToAccelerate = true;
 	}
 
 	void FixedUpdate () {
-		velocity = UpdateVelocity(Vector2.up * 0.001f);
-		position = UpdatePosition(velocity);
-		playerLocation.position = position;
-		
+		velocity = UpdateVelocity(Vector3.up * 0.001f);
+		UpdatePosition(velocity);
+		if (wantsToTurnRight && !wantsToTurnLeft) {
+			Debug.Log("Wants to turn right");
+			Turn(turningSpeed, true);
+		}
+		if (wantsToTurnLeft && !wantsToTurnRight) {
+			Turn(turningSpeed, false);
+		}
+		if (wantsToAccelerate) {
+			Accelerate(acceleration);
+		}
 	}
 
-	Vector2 UpdateVelocity (Vector2 acceleration) {
-		return velocity + acceleration;
+	void ResetControlStates () {
+		wantsToAccelerate = false;
+		wantsToTurnRight = false;
+		wantsToTurnLeft = false;
 	}
 
-	Vector2 UpdatePosition (Vector2 velocity) {
-		return position + velocity;
+	void Turn(float speed, bool right) {
+		Debug.Log("called Turn");
+		playerLocation.Rotate(0.0f, 0.0f, Time.deltaTime * speed);
 	}
+
+	void Accelerate (float acceleration) {
+
+	}
+
+	Vector3 UpdateVelocity (Vector3 acceleration) {
+		return velocity = velocity * dragCoefficient;
+	}
+
+	void UpdatePosition (Vector3 velocity) {
+		playerLocation.position += velocity;
+	}
+
 }

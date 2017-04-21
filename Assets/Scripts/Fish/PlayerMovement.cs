74a824bcba112565dd1247ecfac5667a50d04f0a
Oleg Lenive <olenive@gmail.com>
2017-04-22 00:11:00 +0200

    Add acceleration and non-linear response to turning

--- Assets/Scripts/Fish/PlayerMovement.cs
+++ Assets/Scripts/Fish/PlayerMovement.cs
@@ -6,56 +6,92 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	// Controls
-	public string keyAccelerate = "w";
+	public string keyAccelerate = "w"; // This will be removed from the relase version
 	public string keyTurnLeft = "a";
 	public string keyTurnRight = "d";
+	public string keyFastTurn = "space";
 
 	// Player position etc
 	Rigidbody playerRigidbody;
 	public Vector3 velocity;
 
 	// Player stats
-	public float acceleration = 0.001f;
-	public float turningSpeed = 100f;
-	public float dragCoefficient = 0.1f;
+	public float acceleration = 0.01f;
+	public float dragCoefficient = 0.95f;
 	public float minimumSpeed = 0.01f;
+	public float normalTurningSpeed = 500f;
+	public float fastTurningSpeed = 1000f;
+	public float accelerationCutoff = 0.5f;  // As the fish turn the turning speed factor goes from 0 to 1.  This cutoff indicates the minimum turning speed factor that produces the maximum acceleration.
+	float turningSpeed;
+
+	// Turning sigmoid function parameters
+	// 1/(1+e^( turnDelay - turnResponsiveness * x))
+	public float turnDelay = 5f;
+	public float turnResponsiveness = 8f;
 
 	// Player states
 	bool wantsToTurnLeft;
 	bool wantsToTurnRight;
 	bool wantsToAccelerate;
+	float timeAfterTurnButtonDown = 0.0f;
+	float timeAfterTurnButtonUp = 0.0f;
+	float wiggleAcceleration = 0.0f;
+	// bool turnButtonIsPressed = false;
 
 	void Awake () {
 		playerRigidbody = GetComponent<Rigidbody> ();
+		turningSpeed = normalTurningSpeed;
 	}
 
 	// Use this for initialization
 	void Start () {
-
+		timeAfterTurnButtonUp = 1f;
+		timeAfterTurnButtonDown = 0f;
+		velocity = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		ResetControlStates();
+		// Rect to player input
 		if (Input.GetKey(keyTurnLeft)) wantsToTurnLeft = true;
 		if (Input.GetKey(keyTurnRight)) wantsToTurnRight = true;
 		if (Input.GetKey(keyAccelerate)) wantsToAccelerate = true;
+		if (Input.GetKey(keyFastTurn)) {
+			turningSpeed = fastTurningSpeed;
+		} else {
+			turningSpeed = normalTurningSpeed;	
+		}
+		// Reset turn button timers
+		if (Input.GetKeyDown(keyTurnRight) || Input.GetKeyDown(keyTurnLeft)) timeAfterTurnButtonDown = 0.0f;
+		if (Input.GetKeyUp(keyTurnRight) || Input.GetKeyUp(keyTurnLeft)) timeAfterTurnButtonUp = 0.0f;
 	}
 
 	void FixedUpdate () {
 		// Apply drag
-		velocity = UpdateVelocity(Vector3.up * 0.001f);
+		velocity = UpdateVelocity(velocity);
 		
+		// Update time since turn button was pressed
+		if (timeAfterTurnButtonDown >= 0.0f && (wantsToTurnLeft || wantsToTurnRight)) timeAfterTurnButtonDown += Time.deltaTime;
+
+		// Update time since turn button was released
+		if (timeAfterTurnButtonUp >= 0.0f && !(wantsToTurnLeft || wantsToTurnRight)) timeAfterTurnButtonUp += Time.deltaTime;
+
 		// Apply changes due to player input
 		if (wantsToTurnRight && !wantsToTurnLeft) {
-			Turn(turningSpeed, true);
+			Turn(turningSpeed * CalculateTurningSpeedFactor(timeAfterTurnButtonDown, timeAfterTurnButtonUp), true);
 		}
 		if (wantsToTurnLeft && !wantsToTurnRight) {
-			Turn(turningSpeed, false);
+			Turn(turningSpeed * CalculateTurningSpeedFactor(timeAfterTurnButtonDown, timeAfterTurnButtonUp), false);
 		}
 		if (wantsToAccelerate) {
-			velocity = Accelerate(acceleration);
+			velocity = Accelerate(velocity, acceleration);
 		}
+
+		wiggleAcceleration = CalculateWiggleAcceleration (acceleration, CalculateTurningSpeedFactor(timeAfterTurnButtonDown, timeAfterTurnButtonUp), accelerationCutoff);
+		
+		Debug.Log("wiggleAcceleration = " + wiggleAcceleration);
+		Accelerate(velocity,  wiggleAcceleration);
 
 		// Move the player collider
 		UpdatePosition(velocity);
@@ -79,17 +115,36 @@ public class PlayerMovement : MonoBehaviour {
 		
 	}
 
-	Vector3 Accelerate (float acceleration) {
-		// Debug.Log (playerRigidbody.forward);
-		// playerRigidbody.position += playerRigidbody.forward *= 0.1f;
-		return velocity += playerRigidbody.transform.forward * 0.1f;
+	// Function for calculating current turning speed based on time since the turn button was pressed or released
+	float CalculateTurningSpeedFactor (float t1, float t2) {
+		float delta = t1 - t2;
+		if (delta < 0.0f) {
+			delta = 0f;
+			timeAfterTurnButtonUp = 0f;
+		}
+		return 1f / (1f + Mathf.Exp( turnDelay - turnResponsiveness * delta ));
 	}
 
-	Vector3 UpdateVelocity (Vector3 acceleration) {
-		return velocity = velocity * dragCoefficient;
+	float CalculateWiggleAcceleration (float acceleration, float turningSpeedFactor, float threshold) {
+		if (turningSpeedFactor >= threshold) {
+			return acceleration;
+		} else {
+			return (turningSpeedFactor - threshold) * acceleration;
+		}
+	}
+
+	Vector3 Accelerate (Vector3 velocity, float acceleration) {
+		// Debug.Log (playerRigidbody.forward);
+		// playerRigidbody.position += playerRigidbody.forward *= 0.1f;
+		return velocity + playerRigidbody.transform.forward * acceleration;
+	}
+
+	Vector3 UpdateVelocity (Vector3 velocity) {
+		return velocity * dragCoefficient;
 	}
 
 	void UpdatePosition (Vector3 velocity) {
+		Debug.Log(velocity.magnitude);
 		playerRigidbody.MovePosition (transform.position + velocity);
 	}
 

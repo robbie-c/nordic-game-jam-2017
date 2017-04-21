97e98136db5e3fe7154ee9ebc297399924d153f4
Robbie Coomber <robbie.coomber@gmail.com>
2017-04-21 15:41:37 +0200

    Server communication can send and receive

---
+++ Assets/Scripts/ClientMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class ClientMessage
+	{
+		public readonly string text;
+
+		public ClientMessage (string text)
+		{
+			this.text = text;
+		}
+	}
+}
+

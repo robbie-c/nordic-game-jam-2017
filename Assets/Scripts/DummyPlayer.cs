fc2fa3f83cfb98afc887206dbd4bd761e48abf26
Robbie Coomber <robbie.coomber@gmail.com>
2017-04-21 16:14:43 +0200

    Add a DummyPlayer script for manually testing client server comms

---
+++ Assets/Scripts/DummyPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AssemblyCSharp;
+
+public class DummyPlayer : MonoBehaviour {
+
+	private ServerCommunication serverCommunication;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (serverCommunication == null) {
+			serverCommunication = ServerCommunication.GetRoot ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			serverCommunication.SendClientMessage (new ClientMessage ("up"));
+		}
+
+		ServerMessage serverMessage;
+		if (serverCommunication.TryGetServerMessage(out serverMessage)) {
+			Debug.Log(serverMessage.text);
+		}
+	}
+}

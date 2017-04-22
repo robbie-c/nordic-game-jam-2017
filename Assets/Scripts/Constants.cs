af3ece4ba859ede474839f79c47d7d0442fe87e1
Robbie Coomber <robbie.coomber@gmail.com>
2017-04-22 15:27:45 +0200

    Use Robbie's machine as default server

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,7 +4,7 @@ namespace AssemblyCSharp
 {
 	public static class Constants
 	{
-		public const string kServerAddr = "127.0.0.1";
+		public const string kServerHostname = "saffron.local";
 		public const int kServerPort = 41234;
 		public const int kClientPort = 41235;
 		public const int kGameStateUpdateTickMs = 500;

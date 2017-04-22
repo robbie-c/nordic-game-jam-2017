b07a581e623166ad54658199f9a4e943844fd365
Robbie Coomber <robbie.coomber@gmail.com>
2017-04-22 21:25:08 +0200

    Send a ServerToClientStartMessage automatically after some time after people are frozen
    This controlled by constants, and is currently 10 + 5 seconds after the first person is frozen, or 5 seconds after the last person is frozen.

--- server/gameState/sagas.js
+++ server/gameState/sagas.js
@@ -1,17 +1,21 @@
-import { call, put, takeEvery, throttle, select } from 'redux-saga/effects';
+import { call, put, takeEvery, takeLatest, throttle, select } from 'redux-saga/effects';
 
 import * as actions from './actions';
 import {
   selectUdpServer,
   selectPlayers,
-  selectGameId
+  selectGameId,
+  selectAnyPlayersHidden
 } from './selectors';
 import {
   udpSend,
-  wsSend
+  wsSend,
+  delaySeconds
 } from './calls';
 import {
-  kGameStateUpdateTickMs
+  kGameStateUpdateTickMs,
+  kSecondsBetweenFirstHideAndRoundEnd,
+  kSecondsBetweenRoundEndAndNextRoundStart
 } from '../constants';
 
 function * wsConnection ({ client }) {
@@ -94,6 +98,7 @@ function * playerStateUpdate () {
   const udpServer = yield select(selectUdpServer);
   const players = yield select(selectPlayers);
   const gameId = yield select(selectGameId);
+  const anyPlayersHidden = yield select(selectAnyPlayersHidden);
 
   const message = JSON.stringify({
     type: 'ServerGameStateMessage',
@@ -114,9 +119,36 @@ function * playerStateUpdate () {
       yield call(udpSend, udpServer, player.udpAddr, player.udpPort, message);
     }
   }
+
+  if (!anyPlayersHidden) {
+    const newAnyPlayersHidden = players.some(player => player.frozen);
+    if (newAnyPlayersHidden) {
+      yield put({type: actions.FIRST_PLAYER_HIDDEN});
+    }
+  } else {
+    const allPlayersHidden = players.every(player => player.frozen);
+    if (allPlayersHidden) {
+      yield put({type: actions.LAST_PLAYER_HIDDEN});
+    }
+  }
 }
 
-function * adminStartGame () {
+function * timeoutGameEnd (action) {
+  switch (action.type) {
+    case actions.FIRST_PLAYER_HIDDEN:
+      call(delaySeconds, kSecondsBetweenFirstHideAndRoundEnd + kSecondsBetweenRoundEndAndNextRoundStart);
+      break;
+    case actions.LAST_PLAYER_HIDDEN:
+      call(delaySeconds, kSecondsBetweenRoundEndAndNextRoundStart);
+      break;
+  }
+
+  if (action.type !== actions.START_GAME) {
+    yield put({type: actions.START_GAME});
+  }
+}
+
+function * startGame () {
   const players = yield select(selectPlayers);
   const gameId = yield select(selectGameId);
 
@@ -136,11 +168,17 @@ function * adminStartGame () {
   }
 }
 
+function * adminStartGame () {
+  yield put({type: actions.START_GAME});
+}
+
 export default function * saga () {
   yield takeEvery(actions.WS_CONNECTION, wsConnection);
   yield takeEvery(actions.WS_MESSAGE, wsMessage);
   yield takeEvery(actions.UDP_MESSAGE, udpMessage);
   yield takeEvery(actions.WS_DISCONNECT, wsDisconnect);
   yield takeEvery(actions.ADMIN_START_GAME, adminStartGame);
+  yield takeEvery(actions.START_GAME, startGame);
+  yield takeLatest([actions.START_GAME, actions.FIRST_PLAYER_HIDDEN, actions.LAST_PLAYER_HIDDEN], timeoutGameEnd);
   yield throttle(kGameStateUpdateTickMs, [actions.ADD_PLAYER, actions.PLAYER_STATE_UPDATE], playerStateUpdate);
 }

9f51942546fb2c4ca0850b222d706c3863b93b51
Robbie Coomber <robbie.coomber@gmail.com>
2017-04-23 07:25:34 +0200

    Speed up tick rate

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -8,7 +8,7 @@ namespace AssemblyCSharp
 		public const string kServerHostname = "saffron.local";
 		public const int kServerPort = 41234;
 		public const int kClientPort = 41235;
-		public const int kGameStateUpdateTickMs = 500;
+		public const int kGameStateUpdateTickMs = 100;
 		public const int kSecondsBetweenFirstHideAndRoundEnd = 10;
 		public const int kSecondsBetweenRoundEndAndNextRoundStart = 5;
 		public const int kNumHidingPlaces = 10;

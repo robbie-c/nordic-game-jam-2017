0393452d044140ef9f861709d02b5c24093bebd1
Robbie Coomber <robbie.coomber@gmail.com>
2017-04-23 10:10:10 +0200

    HACKY MCHACKFACE
    The rotation to the child model was not being applied, so I put in a massive hack to rotate the parent by 90 degrees.
    
    I haven't slept for more than a couple of hours out of the last 60, I suspect awake robbie will be disgusted by this.

--- Assets/Scripts/OtherPlayer.cs
+++ Assets/Scripts/OtherPlayer.cs
@@ -24,7 +24,10 @@ public class OtherPlayer : MonoBehaviour {
 	void Update () {
 		var rigidBody = GetComponent<Rigidbody> ();
 		rigidBody.position = Vector3.Lerp (rigidBody.position, desiredPosition, Time.deltaTime * PositionLerpSpeed);
-		rigidBody.rotation = Quaternion.Lerp (rigidBody.rotation, desiredRotation, Time.deltaTime * RotationLerpSpeed);
+		var hackyAngles = desiredRotation.eulerAngles;
+		hackyAngles.y += -90;
+		var hackyRotation = Quaternion.Euler (hackyAngles);
+		rigidBody.rotation = Quaternion.Lerp (rigidBody.rotation, hackyRotation, Time.deltaTime * RotationLerpSpeed);
 		rigidBody.velocity = Vector3.Lerp (rigidBody.velocity, desiredVelocity, Time.deltaTime * VelocityLerpSpeed);
 	}
 }

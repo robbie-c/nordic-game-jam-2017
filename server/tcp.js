7f5f06e8e0285e3ce8f58e362511f072cea288ae
Robbie Coomber <robbie.coomber@gmail.com>
2017-04-26 21:17:27 +0100

    Host web content from WS port

--- server/tcp.js
+++ server/tcp.js
@@ -1,13 +1,18 @@
 import { Server } from 'ws';
-
 import { kServerPort } from './constants';
+const express = require('express');
+const http = require('http');
 
 let playerIdCounter = 0;
 
 export default function createWsServer (onNewConnection, onMessage, onError, onDisconnect) {
+  const app = express();
+  app.use(express.static('server/html'));
+
+  const httpServer = http.createServer(app);
+
   const server = new Server({
-    perMessageDeflate: false,
-    port: process.env.PORT || kServerPort
+    server: httpServer
   });
 
   server.on('connection', (client) => {
@@ -24,7 +29,9 @@ export default function createWsServer (onNewConnection, onMessage, onError, onD
       onDisconnect(client);
     });
   });
-  server.on('listening', function () {
-    console.log(`ws server listening on port ${kServerPort} ...`);
+
+  const port = process.env.PORT || kServerPort;
+  httpServer.listen(port, function () {
+    console.log(`ws server listening on port ${port} ...`);
   });
 }
